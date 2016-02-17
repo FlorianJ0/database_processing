e77cdbc87f50d124c97ca33dde8a71fe5d31957e
totocaca <pueduponey@gmail.com>
2016-02-17 10:25:40 -0500

    ajout bordel function

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+        name='database_processing',
+        version='1.0',
+        packages=['recalage', 'dicom_treatment'],
+        url='github.com/totocaca',
+        license='nope',
+        author='florian',
+        author_email='',
+        description='du caca'
+)
